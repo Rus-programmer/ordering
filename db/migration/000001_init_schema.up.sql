b17d047a41c3ac223b70762ddcd6d2a4fa85981c
rus <math.it.sport@gmail.com>
2025-02-03 21:08:42 +0500

    add customer role

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,9 +1,11 @@
 CREATE TYPE order_status AS ENUM ('pending', 'confirmed', 'cancelled');
+CREATE TYPE user_role AS ENUM ('user', 'admin');
 
 CREATE TABLE customers
 (
     id              bigserial PRIMARY KEY,
     username        VARCHAR(100) NOT NULL UNIQUE,
+    role            user_role    NOT NULL DEFAULT 'user',
     hashed_password VARCHAR(250) NOT NULL,
     created_at      TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     updated_at      TIMESTAMPTZ  NOT NULL DEFAULT NOW()
@@ -38,38 +40,46 @@ CREATE TABLE order_products
 );
 
 CREATE FUNCTION update_modified_column()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     NEW.updated_at = NOW();
-RETURN NEW;
+    RETURN NEW;
 END;
 $$
-LANGUAGE plpgsql;
+    LANGUAGE plpgsql;
 
 CREATE TRIGGER update_customers_updated_at
     BEFORE UPDATE
     ON customers
-    FOR EACH ROW EXECUTE FUNCTION update_modified_column();
+    FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
 
 CREATE TRIGGER update_products_updated_at
     BEFORE UPDATE
     ON products
-    FOR EACH ROW EXECUTE FUNCTION update_modified_column();
+    FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
 
 CREATE TRIGGER update_orders_updated_at
     BEFORE UPDATE
     ON orders
-    FOR EACH ROW EXECUTE FUNCTION update_modified_column();
+    FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
 
 CREATE OR REPLACE FUNCTION prevent_order_deletion()
-RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
     RAISE EXCEPTION 'Direct deletion of orders is prohibited. Use soft delete (UPDATE).';
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER prevent_order_deletion_trigger
-    BEFORE DELETE ON orders FOR EACH ROW EXECUTE FUNCTION prevent_order_deletion();
+    BEFORE DELETE
+    ON orders
+    FOR EACH ROW
+EXECUTE FUNCTION prevent_order_deletion();
 
 CREATE INDEX idx_products_name ON products (name);
 CREATE INDEX idx_orders_status ON orders (status);
