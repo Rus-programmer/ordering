df398f130489ffe1bb2feb6365e869eceb80333c
rus <math.it.sport@gmail.com>
2025-02-04 15:15:57 +0500

    create tests for generated sqlc queries

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+	"log"
+	util "ordering/utils"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var testStore Store
+
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	defer connPool.Close()
+
+	testStore = NewStore(connPool)
+	os.Exit(m.Run())
+}
